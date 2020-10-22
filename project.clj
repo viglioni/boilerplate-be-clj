eef76c64f0349f56948cbcde523e9a4e7de7b363
Laura <laura.viglioni@loft.com.br>
2020-10-22 10:59:30 -0300

    added jdcd and mysql connector to dependencies

--- project.clj
+++ project.clj
@@ -15,7 +15,9 @@
                  [environ "1.1.0"]
                  [org.clojure/data.json "1.0.0"]
                  [ring/ring-mock "0.4.0"]
-                 [ring/ring-json "0.5.0"]]
+                 [ring/ring-json "0.5.0"]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [mysql/mysql-connector-java "5.1.6"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]
             [lein-ring "0.12.5"]
@@ -27,5 +29,6 @@
   :main ^:skip-aot boilerplate-clojure.server
   :aliases {"test:watch" ["shell" "sh" "scripts/test_dev.sh"]
             "rename-project" ["shell" "sh" "scripts/rename-project.sh"]}
+
   :profiles {:uberjar {:aot :all}
              :production {:env {:production true}}})
